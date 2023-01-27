e0b3b15aab79dbb0a9523ce0cd4319590a3d0809
Omotoke <96818824+GirlWho-Codes@users.noreply.github.com>
2023-01-27 20:36:54 +0100

    Update LoadingState.jsx

--- components/LoadingState.jsx
+++ components/LoadingState.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { LoadingStateWrapper } from '../utils/styles';
+// import React from 'react'
+// import { LoadingStateWrapper } from '../utils/styles';
 
-const LoadingState = () => {
-    return (
-        <LoadingStateWrapper>
-          <div className="first"></div>
-          <div className="second"></div>
-          <div className="last"></div>
-        </LoadingStateWrapper>
-      );
-}
+// const LoadingState = () => {
+//     return (
+//         <LoadingStateWrapper>
+//           <div className="first"></div>
+//           <div className="second"></div>
+//           <div className="last"></div>
+//         </LoadingStateWrapper>
+//       );
+// }
 
-export default LoadingState
+// export default LoadingState
